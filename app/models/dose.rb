15e246d07d5ca33363dc99ac82e0b9f75863c00c
paul cheam <paulccheam@gmail.com>
2018-11-22 18:47:18 +0100

    controllers completed

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  has_many :doses
   validates :description, presence: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
